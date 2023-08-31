d253c47efd81f7db1d1e25bd0e24d79e7a63a3e2
Shea Richardson <shearichardson@btinternet.com>
2023-08-31 16:40:20 +0100

    Making setSkinWeights logic more robust
    setSkinWeights in pymxs  will now handle vertices of various influence sizes
    Still working on pymaya logic.
    Fixed issue with eigen matricies resizing and wiping resetting all previous values, resulting in non-valid boneIDs and crashes when setting weights

---
+++ PYProjects/skin_plus_plus_test/ordering_test.py
@@ -0,0 +1,58 @@
+from __future__ import annotations
+
+
+def get_flat_weights(bone_ids: list[list[int]], weights: list[list[float]], max_influence_count: int = 3):
+    vertex_count = len(bone_ids)
+    m_weights = [0.0] * vertex_count * max_influence_count
+    for vertex_index in range(vertex_count):
+        vertex_bone_ids = bone_ids[vertex_index]
+        array_index = vertex_index * max_influence_count
+        for influence_index in range(len(vertex_bone_ids)):
+            bone_id = vertex_bone_ids[influence_index]
+            if bone_id == -1:
+                continue
+            index = array_index + bone_id
+            m_weights[index] = weights[vertex_index][influence_index]
+
+    return m_weights
+
+names = ["one", "two", "three"]
+new_names = ["FART", "three", "one", "two", "RANDOM"]
+bone_ids = [[0, 1, -1], [-1, 1, 2], [0, 1, 2]]
+weights = [[0.25, 0.75, None], [None, 0.5, 0.5], [0.25, 0.25, 0.5]]
+
+m_weights = get_flat_weights(bone_ids, weights)
+
+print(m_weights)
+print(m_weights == [0.25, 0.75, 0.0, 0.0, 0.5, 0.5, 0.25, 0.25, 0.5])
+
+
+name_map = {}
+for index, name in enumerate(new_names):
+    name_map[name] = index
+
+new_bone_ids = []
+for name in names:
+    new_index = name_map[name]
+    new_bone_ids.append(new_index)
+
+def get_sorted_bone_ids(bone_ids: list[list[int]], new_bone_id_order: list[int]):
+    for vertex in bone_ids:
+        for i, bone_id in enumerate(vertex):
+            if bone_id == -1:
+                continue
+
+            new_index = new_bone_id_order[bone_id]
+            vertex[i] = new_index
+
+    return bone_ids
+
+
+sorted_bone_ids = get_sorted_bone_ids(bone_ids, new_bone_ids)
+print(bone_ids)
+print(bone_ids == [[1, 2, -1], [-1, 2, 0], [1, 2, 0]])
+
+m_weights = get_flat_weights(sorted_bone_ids, weights)
+
+print(m_weights)
+print(m_weights == [0.0, 0.25, 0.75, 0.5, 0.0, 0.5, 0.5, 0.25, 0.25])
