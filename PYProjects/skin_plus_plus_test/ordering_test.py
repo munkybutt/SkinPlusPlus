b5c8751084c4046df70d5bf4293adb81c7ddef54
Shea Richardson <7100563+munkybutt@users.noreply.github.com>
2023-09-14 22:56:28 +0100

    Update ordering_test.py with functional logic

--- PYProjects/skin_plus_plus_test/ordering_test.py
+++ PYProjects/skin_plus_plus_test/ordering_test.py
@@ -18,13 +18,13 @@ def get_flat_weights(bone_ids: list[list[int]], weights: list[list[float]], max_
 
 names = ["one", "two", "three"]
 new_names = ["FART", "three", "one", "two", "RANDOM"]
-bone_ids = [[0, 1, -1], [-1, 1, 2], [0, 1, 2]]
-weights = [[0.25, 0.75, None], [None, 0.5, 0.5], [0.25, 0.25, 0.5]]
+bone_ids = [[0, 1, -1], [1, 2, -1], [0, 1, 2]]
+weights = [[0.25, 0.75, None], [0.5, 0.5, None], [0.25, 0.25, 0.5]]
 
 m_weights = get_flat_weights(bone_ids, weights)
 
-print(m_weights)
-print(m_weights == [0.25, 0.75, 0.0, 0.0, 0.5, 0.5, 0.25, 0.25, 0.5])
+# print(m_weights)
+# print(m_weights == [0.25, 0.75, 0.0, 0.5, 0.5, 0.0, 0.25, 0.25, 0.5])
 
 
 name_map = {}
@@ -36,6 +36,7 @@ for name in names:
     new_index = name_map[name]
     new_bone_ids.append(new_index)
 
+# print(new_bone_ids)
 def get_sorted_bone_ids(bone_ids: list[list[int]], new_bone_id_order: list[int]):
     for vertex in bone_ids:
         for i, bone_id in enumerate(vertex):
@@ -49,10 +50,15 @@ def get_sorted_bone_ids(bone_ids: list[list[int]], new_bone_id_order: list[int])
 
 
 sorted_bone_ids = get_sorted_bone_ids(bone_ids, new_bone_ids)
-print(bone_ids)
-print(bone_ids == [[1, 2, -1], [-1, 2, 0], [1, 2, 0]])
+# print(f"sorted_bone_ids: {sorted_bone_ids}")
 
-m_weights = get_flat_weights(sorted_bone_ids, weights)
+m_weights = get_flat_weights(sorted_bone_ids, weights, max_influence_count=len(new_names))
 
-print(m_weights)
-print(m_weights == [0.0, 0.25, 0.75, 0.5, 0.0, 0.5, 0.5, 0.25, 0.25])
+print(f"m_weights: {m_weights}")
+for i in range(3):
+    span = 5 * i
+    vert = m_weights[span:span+5]
+    for j, weight in enumerate(vert):
+        print(f"{new_names[j]}: {weight}")
+    print("-----")
+
